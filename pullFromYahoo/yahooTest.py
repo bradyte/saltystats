f46a990e902345e74af8c1a62b0a1223c78566e7
Tom Brady <you@example.combradyte@gmail.com>
2017-11-20 09:16:00 -0500

    player ids of dsts

--- pullFromYahoo/yahooTest.py
+++ pullFromYahoo/yahooTest.py
@@ -4,6 +4,8 @@ import math
 import matplotlib.pyplot as plt
 from func import *
 from displayStats import *
+import pprint
+import time
 
 ## For my personal history:
 ## 2014 - 455893
@@ -16,10 +18,10 @@ from displayStats import *
 ## https://jsonformatter.curiousconcept.com/
 
 ## This is the path to your Yahoo API consumer and secret key
-filePath        = '/Users/tombrady/drive/sw/oauth2.json'
+filePath        = '/Users/tbrady/drive/sw/json/yahoo/oauth2.json'
 oauthToken      = beginOauth2Session(filePath)
 
-season          = 2017
+season          = 2013
 league_id       = 470610
 team_id         = 1
 week            = 1
@@ -36,7 +38,7 @@ player_id = 29236
 
 
 
-leagueSettings  = getLeagueSettings(season, league_id, oauthToken)
+#leagueSettings  = getLeagueSettings(season, league_id, oauthToken)
 #type=week;week={week}
 fpts        = []
 fptsArr     = []
@@ -52,44 +54,59 @@ confInt     = 1.96  # z* of 95%
 confLimits  = 0.0
 
 
-title       = getPlayerName(season, player_id, week, oauthToken)
+player_id = 100019
+#for i in range(player_id,player_id+32):
+url = 'https://fantasysports.yahooapis.com/fantasy/v2/player/'  \
+    + str(getSeasonGameKey(season)) + '.p.' + str(player_id)    \
+    + '/stats;type=week;week=' + str(week) + '?format=json'
+jsondata = jsonQuery(url, oauthToken)
+#    jsondataText = jsondata['fantasy_content']['player'][0][2]['name']['full']
+#    jsondataStats = jsondata['fantasy_content']['player'][1]['player_stats']['stats']
+#print(i)
+pprint.pprint(jsondata)
+#    time.sleep(2)
 
-print('Week:     Fpts      Average   Stdev     CV        ROC       Margin+/-') 
-for i in range(1, int(leagueSettings.Dates.current_week)):
-    fpts.append(getPlayerStats(season, player_id, i, leagueSettings.Scoring.statInfo.value, oauthToken))
-    if fpts[i-1] != 'BYE':
-        fptsArr.append(fpts[i-1])
-        fptsMean    = round(numpy.mean(fptsArr), 2)
-        fptsStdev   = round(numpy.std(fptsArr), 2)
-        if i == 1:
-            fptsCV      = 0.0
-            fptsROC     = 0.0
-            fptsEXP     = 0.0
-        else:
-            fptsCV      = round(fptsStdev/fptsMean, 2)
-            fptsROC     = round(fpts[i-1]/fptsMean, 2)
-            fptsSE      = round(confInt*fptsStdev/numpy.sqrt(i),2)
-#            fptsVar     = round(fptsROC/fptsStdev,3)
-        plt.subplot(211)    
-        plt.scatter(i, fpts[i-1])
-        plt.xlabel('Week')
-        plt.title(title)
-        plt.axis([0, 17, 0, 40]) 
-        plt.grid(True)
-        
-        plt.subplot(212) 
-        plt.scatter(i, fptsVar)
-        plt.xlabel('Week')
-        plt.title('CV')
-        plt.axis([0, 17, 0, 0.6]) 
-        plt.grid(True)
-    print('{:<10}{:<10}{:<10}{:<10}{:<10}{:<10}{:<10}'.format(\
-          i,fpts[i-1], fptsMean, fptsStdev, fptsCV, fptsROC, fptsSE))
+    
 
- 
+#title       = getPlayerName(season, player_id, week, oauthToken)
+#print('Week:     Fpts      Average   Stdev     CV        ROC       Margin+/-') 
+#for i in range(1, int(leagueSettings.Dates.current_week)):
+#    fpts.append(getPlayerStats(season, player_id, i, leagueSettings.Scoring.statInfo.value, oauthToken))
+#    if fpts[i-1] != 'BYE':
+#        fptsArr.append(fpts[i-1])
+#        fptsMean    = round(numpy.mean(fptsArr), 2)
+#        fptsStdev   = round(numpy.std(fptsArr), 2)
+#        if i == 1:
+#            fptsCV      = 0.0
+#            fptsROC     = 0.0
+#            fptsEXP     = 0.0
+#        else:
+#            fptsCV      = round(fptsStdev/fptsMean, 2)
+#            fptsROC     = round(fpts[i-1]/fptsMean, 2)
+#            fptsSE      = round(confInt*fptsStdev/numpy.sqrt(i),2)
+##            fptsVar     = round(fptsROC/fptsStdev,3)
+#        plt.subplot(211)    
+#        plt.scatter(i, fpts[i-1])
+#        plt.xlabel('Week')
+#        plt.title(title)
+#        plt.axis([0, 17, 0, 40]) 
+#        plt.grid(True)
+#        
+#        plt.subplot(212) 
+#        plt.scatter(i, fptsVar)
+#        plt.xlabel('Week')
+#        plt.title('CV')
+#        plt.axis([0, 17, 0, 0.6]) 
+#        plt.grid(True)
+#    print('{:<10}{:<10}{:<10}{:<10}{:<10}{:<10}{:<10}'.format(\
+#          i,fpts[i-1], fptsMean, fptsStdev, fptsCV, fptsROC, fptsSE))
+#
+# 
+#
+#plt.subplots_adjust(wspace=0.8, hspace=0.8)
+#plt.show()
 
-plt.subplots_adjust(wspace=0.8, hspace=0.8)
-plt.show()
+
 #needed to initialize class
 #leagueSettings.About.num_teams
 #leagueSettings.Dates.start_week
@@ -141,20 +158,10 @@ plt.show()
 ##    ]  
 ##})
 
-
- 
-
-
 #roster          = getWeeklyRoster(season, league_id, team_id, week, roster_size, oauthToken)
-
 #matchups         = getWeeklyMatchup(season, league_id, team_id, week, leagueSettings.About.num_teams, oauthToken)
-
 #printCleanMatchups(matchups, leagueSettings.About.num_teams)
-
 #print(json.dumps(tmp, indent=2))
-
-
-
 
 
 #seeAbout       = 1
@@ -164,17 +171,3 @@ plt.show()
 #printCleanSettings(leagueSettings,seeAbout,seeDates,seeScoring)
 #
 #printCleanRoster(roster, roster_size)
-
-
-## still need to figure out week 13 game_id!!
-#for i in range(273,331):
-#    league_id   = 470610
-#    team_id     = 1
-#    
-#    url     = 'https://fantasysports.yahooapis.com/fantasy/v2/teams;team_keys='  \
-#                    + str(i) +'.l.' + str(league_id) + '.t.' + str(team_id) + \
-#                    '/roster;week=1?format=json'                        
-#    jsondata = jsonQuery(url, oauthToken)
-#    print (jsondata)
-#    print (i)
-#    time.sleep(1)
